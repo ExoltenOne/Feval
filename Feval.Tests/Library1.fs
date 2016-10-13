f6d6b37f3aae524294236a4ec1f23191ebfe7b22
Krzysztof Madej <krzysztof.madej.1987@gmail.com>
2016-10-13 07:20:24 +0200

    Added initial version

---
+++ Feval.Tests/Library1.fs
@@ -0,0 +1,12 @@
+﻿namespace Feval.Tests
+
+open Feval
+open Xunit
+
+type Class2() = 
+    member this.X = "F#"
+
+    [<Fact>]
+    member this.``Test`` () =
+        let c = new Class1()
+        c.X = "F#"
